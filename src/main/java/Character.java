6fa1ffcd5ea7ea0f6a8366b1e7b1b1ff16977f30
vicrz <vicrzamora@gmail.com>
2026-04-02 16:30:36 -0500

    retobit finalizado

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,14 +1,43 @@
-class Character implements Combatant {
+abstract class Character implements Combatant {
     // 🗒️ PROPIEDADES
     protected String name;
     protected int health;
-    // le falta el status
+    protected Status status;
 
     // 🏗️ CONSTRUCTOR
     protected Character(String name, int health) {
         this.name = name;
         this.health = health;
-        // el status inicial debe ser siempre REGULAR
+        this.status = Status.REGULAR;
     }
 
+    @Override
+    public void receiveDamage(int damage) {
+        this.health -= damage;
+
+        if (!this.isAlive()) {
+            this.status = Status.DEAD;
+            this.health = 0;
+        }
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    @Override
+    public int getCurrentHealth() {
+        return this.health;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getStatus() {
+        return this.status.name();
+    }
 }
